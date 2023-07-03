1247775e5deec28a80ef7e02a941133c2aed8282
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2023-07-03 15:30:50 -0300

    Refatoração sintaxe verbos (getmappin, deleteMappin, etc)

--- src/main/java/com/example/restwithspringbootandjava/controller/PersonController.java
+++ src/main/java/com/example/restwithspringbootandjava/controller/PersonController.java
@@ -5,6 +5,7 @@ import com.example.restwithspringbootandjava.model.Person;
 import com.example.restwithspringbootandjava.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,43 +18,45 @@ import java.util.List;
 @RequestMapping("/person")
 public class PersonController {
 
-    //Autowired permite a instanciação da classe pelo springboot em run time
+
     @Autowired
     private PersonServices services;
 
+    /*Refatoração do RequestMappin para cada um dos Verbos HTTP
+    * deixando o codigo mais limpo e facil para leitura: Getmappin, PutMappin, etc....*/
 
-    @RequestMapping(value = "/{id}",
-                    method = RequestMethod.GET,
-                    produces = MediaType.APPLICATION_JSON_VALUE)
+
+    @GetMapping(value = "/{id}",
+               produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById (@PathVariable(value = "id") Long id){
         return services.findById(id);
     }
 
-    @RequestMapping(method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll () {
         return services.findAll();
     }
 
 
-    @RequestMapping(method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person create (@RequestBody Person person) {
         return services.create(person);
     }
 
-    @RequestMapping(method = RequestMethod.PUT,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person update (@RequestBody Person person) {
         return services.update(person);
     }
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.DELETE)
-    public void delete (@PathVariable(value = "id") Long id){
+
+    /*O retorno de uma ResponseEntity permite alterar o codigo do status mais adequado, no caso do
+    * delete, o 204 - noContent - é mais adequado do que o 200*/
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete (@PathVariable(value = "id") Long id){
         services.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
